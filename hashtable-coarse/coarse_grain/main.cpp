1025fb39e7cdae3e20f57ddf8e4a347ff500992c
Uzochi Dimkpa <uzochidimkpa@gmail.com>
2023-02-17 19:49:58 -0500

    activity-hashtable-coarse-6

--- hashtable-coarse/coarse_grain/main.cpp
+++ hashtable-coarse/coarse_grain/main.cpp
@@ -78,27 +78,34 @@ int main(int argc, char **argv)
 
 
   // TODO: write code here
-  std::vector<std::thread> threadbatch; std::mutex mtx;
-  // mtx.lock();
+  // Start Timer
+  auto start =std::chrono::steady_clock::now();
+
+  std::vector<std::thread> threadbatch;
+  std::mutex mtx;
 
   for (auto & filecontent: wordmap) {
     std::thread thd;
+    // std::mutex mtx;
 
     thd = std::thread (
       [&mtx, &filecontent, &dict]() {
         for (auto & w : filecontent) {
-          mtx.lock();
+          // std::mutex new_mtx;
+          // std::lock_guard<std::mutex> lg(std::ref(new_mtx));
+
+          std::lock_guard<std::mutex> lg(std::ref(mtx));
+          // mtx.lock();
 
           int count = dict.get(w);
           ++count;
           dict.set(w, count);
 
-          mtx.unlock();
+          // mtx.unlock();
         }
       }
     );
 
-    // mtx.unlock();
     threadbatch.push_back(std::move(thd));
   }
 
@@ -107,6 +114,12 @@ int main(int argc, char **argv)
       t.join();
     }
   }
+
+  // Stop Timer
+  auto stop = std::chrono::steady_clock::now();
+  std::chrono::duration<double> time_elapsed = stop-start;
+
+  std::cerr << time_elapsed.count()<<"\n";
 
 
 
